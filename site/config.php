041ee7d76b4184e6532f0a71917ad9e1697c5ba9
talrez11 <tal@way2cy.com>
2015-08-12 09:50:59 +0300

    Added default language

--- site/config.php
+++ site/config.php
@@ -17,8 +17,8 @@ define('DEBUG', 1);
 // define('SQL_DEBUG', 1);
 
 // site language configuration
-$available_languages = array('en');
-$default_language = 'en';
+$available_languages = array('he');
+$default_language = 'he';
 
 // default session options
 $session_type = Session::TYPE_BROWSER;

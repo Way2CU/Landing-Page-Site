ad78db70d05509230853376067180d19a85d3b6f
Mladen Mijatov <meaneye.rcf@gmail.com>
2015-08-20 14:54:37 +0200

    Pulled Tal on Tal.

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -52,60 +52,34 @@ Site.on_load = function() {
 	if (Site.is_mobile())
 		Site.mobile_menu = new Caracal.MobileMenu();
 
-	 var desktop_gallery = new Caracal.Gallery.Slider();
-	  	 desktop_gallery
-					  	.images.add('img.desktop')
-					  	.images.set_container('div.figures figure')
-					 	.images.set_visible_count(1)
-					 	.controls.set_pause_on_hover(false)
-					 	.controls.set_auto(4000);
-
-
-	 var mobile_gallery = new Caracal.Gallery.Slider();
-	  	 mobile_gallery
-					  	.images.add('img.mobile')
-					  	.images.set_container('div.figures div.phone')
-					 	.images.set_visible_count(1)
-					 	.controls.set_pause_on_hover(false)
-					 	.controls.set_auto(4000);
+	 var desktop_gallery = new Caracal.Gallery.Slider(); desktop_gallery
+	 	 .images.add('img.desktop') .images.set_container('div.figures figure')
+	 	 .images.set_visible_count(1) .controls.set_pause_on_hover(false)
+	 	 .controls.set_auto(4000); var mobile_gallery = new
+	 	 Caracal.Gallery.Slider(); mobile_gallery .images.add('img.mobile')
+	 	 .images.set_container('div.figures div.phone')
+	 	 .images.set_visible_count(1) .controls.set_pause_on_hover(false)
+	 	 .controls.set_auto(4000);
 	// Clients Gallery
-	 var client_gallery = new Caracal.Gallery.Slider();
-  	 client_gallery
-				  	.images.add('img.clients')
-				  	.images.set_container('div.client_gallery')
-				 	.images.set_visible_count(6)
-				 	.images.set_spacing(100)
-				 	.images.set_step_size(1)
-	 	            .images.set_center(true)
-            		.images.set_spacing(20)
-				 	.controls.set_pause_on_hover(false)
-				 	.controls.attach_next('a.btn_next')
-				 	.controls.attach_previous('a.btn_previous');
-
-	gallery = new PageControl('div.sites_container','div.site')
-	gallery.attachPreviousControl($('a.previous'))
-	gallery.attachNextControl($('a.next'))
-	.setWrapAround(true);
-
-	var version_links = $('div.controls a');
-	var container = $('div.sites_container');
-	container.attr('class',"sites_container desktop");
-	$('div.controls a.desktop').addClass('active');
-
-	version_links.on('click',function(){
-		var item = $(this);
-		item.addClass('active');
-		version_links.not(item).removeClass('active');
-		var newClass = item.attr('class');
-		container.attr('class',"sites_container " + newClass);
-
-	});
-
-
-
-
+	 var client_gallery = new Caracal.Gallery.Slider(); client_gallery
+	 	 .images.add('img.clients') .images.set_container('div.client_gallery')
+	 	 .images.set_visible_count(6) .images.set_spacing(100)
+	 	 .images.set_step_size(1) .images.set_center(true)
+	 	 .images.set_spacing(20) .controls.set_pause_on_hover(false)
+	 	 .controls.attach_next('a.btn_next')
+	 	 .controls.attach_previous('a.btn_previous'); gallery = new
+	 	 PageControl('div.sites_container','div.site')
+	 	 gallery.attachPreviousControl($('a.previous'))
+	 	 gallery.attachNextControl($('a.next')) .setWrapAround(true); var
+	 	 version_links = $('div.controls a'); var container =
+	 	 $('div.sites_container'); container.attr('class',"sites_container
+	 	 	 desktop"); $('div.controls a.desktop').addClass('active');
+	 version_links.on('click',function(){ var item = $(this);
+	 	 item.addClass('active');
+	 	 version_links.not(item).removeClass('active'); var newClass =
+	 	 	 item.attr('class'); container.attr('class',"sites_container " +
+	 	 	 	 newClass); });
 };
-
 
 // connect document `load` event with handler function
 $(Site.on_load);
